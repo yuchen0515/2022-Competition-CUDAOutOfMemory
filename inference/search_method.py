9e6ed1cbf36baf28481d3ce71445ee2bcf0c5ce1
Navi <ivan000105@gmail.com>
2022-07-03 00:50:25 +0800

    first commit of model/inference/preprocess

---
+++ inference/search_method.py
@@ -0,0 +1,53 @@
+import re
+import torch
+from torch.utils.data import Dataset, DataLoader
+from einops import rearrange
+
+def generate_mask(length):
+    mask = rearrange(torch.triu(torch.ones(length, length)) == 1, 'h w -> w h')
+    mask = mask.float().masked_fill(mask == 0, float('-inf'))
+    mask = mask.masked_fill(mask == 1, float(0.0))
+    
+    return mask
+
+def greedy_search(model, tokenizer, device,
+                  batch, batch_size,
+                  max_len:int=128, 
+                  cls_token_id:int=101, sep_token_id:int=102, pad_token_id:int=0):
+    model.eval()
+    
+    src_input_ids = batch[0].to(device)
+    src_key_padding_mask = batch[1].to(device)
+    src_mask = generate_mask(src_input_ids.shape[1]).to(device)
+    memory_key_padding_mask = src_key_padding_mask
+
+    corrected_sentence_list = []
+    is_done = [False] * batch_size
+    predict_sentence_ids = torch.ones(batch_size, 1).fill_(cls_token_id).type_as(src_input_ids.data)
+    for i in range(max_len - 1):
+        trg_mask = generate_mask(predict_sentence_ids.shape[1]).to(device)
+        output = model.forward(src_input_ids, 
+                               predict_sentence_ids, 
+                               src_key_padding_mask=src_key_padding_mask, 
+                               trg_key_padding_mask=None,
+                               src_mask=src_mask,
+                               trg_mask=trg_mask,
+                               memory_key_padding_mask = memory_key_padding_mask)
+        output = output[:, -1, :]
+        _, next_word = torch.max(output, dim=1)
+        #next_word = next_word.data[0]
+        # 101=[CLS] 102 = [SEP]
+        predict_sentence_ids = torch.cat([predict_sentence_ids, next_word.view(-1, 1)], dim=1)
+        for i in range(len(next_word)):
+            if next_word[i] == sep_token_id:
+                is_done[i] = True
+
+        if all(is_done):
+            break
+
+    for ids in predict_sentence_ids:
+        tokens = ''.join(tokenizer.convert_ids_to_tokens(ids))
+        tokens = re.sub(r'\[CLS]|\[SEP]|\[PAD]\[UNK]', '', tokens)
+        corrected_sentence_list.append(tokens)
+    
+    return corrected_sentence_list
---
+++ model/model.py
@@ -0,0 +1,88 @@
+import math
+import torch
+import torch.nn as nn
+from torch.nn.modules import transformer
+
+
+class PositionEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        #奇數
+        pe[:, 0::2] = torch.sin(position * div_term)
+        #偶數
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+    
+
+class Embedding(nn.Module):
+    def __init__(self, d_model, num_of_vocab):
+        super(Embedding, self).__init__()
+        self.lut = nn.Embedding(num_of_vocab, d_model)
+        self.d_model = d_model
+    
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class GecTransformer(nn.Module):
+    def __init__(self, max_len: int = 200,num_of_vocab: int=22399, d_model: int = 512, nhead: int = 8, num_encoder_layers: int = 4,
+                 num_decoder_layers: int = 4, dim_feedforward: int = 1024, dropout: float = 0.1,
+                 activation: str = "relu"):
+        super(GecTransformer, self).__init__()
+        
+        #encoder
+        encoder_norm = transformer.LayerNorm(d_model)
+        encoder_layer = transformer.TransformerEncoderLayer(d_model, 
+                                                            nhead, 
+                                                            dim_feedforward, 
+                                                            dropout,
+                                                            activation)
+        self.encoder = transformer.TransformerEncoder(encoder_layer, 
+                                                      num_encoder_layers,
+                                                      encoder_norm)
+        
+        #decoder
+        decoder_norm = transformer.LayerNorm(d_model)
+        decoder_layer = transformer.TransformerDecoderLayer(d_model,
+                                                            nhead, 
+                                                            dim_feedforward,
+                                                            dropout,
+                                                            activation)
+        self.decoder = transformer.TransformerDecoder(decoder_layer,
+                                                      num_decoder_layers,
+                                                      decoder_norm)
+        #
+        self.embedding = Embedding(d_model, num_of_vocab)
+        self.position_encoding = PositionEncoding(d_model=d_model, max_len=max_len)
+        self.projection = nn.Linear(d_model, num_of_vocab, bias=False)
+    
+    def forward(self, src, trg, src_key_padding_mask, trg_key_padding_mask, src_mask, trg_mask, memory_key_padding_mask):
+        src_embedding = self.embedding(src)
+        src_embedding = self.position_encoding(src_embedding)        
+        src_embedding = src_embedding.permute(1, 0, 2)
+        
+        memory = self.encoder(src_embedding, 
+                              mask=src_mask, 
+                              src_key_padding_mask=src_key_padding_mask)
+        
+        trg_embedding = self.embedding(trg)
+        trg_embedding = self.position_encoding(trg_embedding)
+        trg_embedding = trg_embedding.permute(1, 0, 2)
+        
+        decoder_output = self.decoder(trg_embedding,
+                                      memory,
+                                      tgt_mask=trg_mask, 
+                                      tgt_key_padding_mask=trg_key_padding_mask,
+                                      memory_key_padding_mask=memory_key_padding_mask)
+        decoder_output = decoder_output.permute(1, 0, 2)
+        output = self.projection(decoder_output)
+        return output
